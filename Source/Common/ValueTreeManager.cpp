aa01205cd7ccaca98b549f417b5c63b8abd708b1
Subnite <vanrietilias@hotmail.com>
2024-09-08 04:58:07 +0200

    cleaned up the value tree class, and added a more sensible default state

--- Source/Common/ValueTreeManager.cpp
+++ Source/Common/ValueTreeManager.cpp
@@ -24,22 +24,45 @@ duck::vt::Property duck::vt::ValueTree::getPropertyType(const juce::Identifier& 
     return duck::vt::Property::UNDEFINED;
 }
 
+juce::Identifier duck::vt::ValueTree::getIDFromType(const duck::vt::Tree& tree) {
+    switch (tree) {
+        case duck::vt::Tree::ROOT : return {"HentaiDuckRoot"}; break;
+        case duck::vt::Tree::CURVE_DATA : return {"CurveData"}; break;
+        case duck::vt::Tree::NORMALIZED_POINTS : return {"NormalizedPoints"}; break;
+        case duck::vt::Tree::POINT : return {"Point"}; break;
+        case duck::vt::Tree::COORDS : return {"Coords"}; break;
+        default: return {"Undefined"}; break;
+    }
+}
+
+juce::Identifier duck::vt::ValueTree::getIDFromType(const duck::vt::Property& property) {
+    switch (property) {
+        case duck::vt::Property::POWER : return {"Power"}; break;
+        case duck::vt::Property::MAX_ABSOLUTE_POWER : return {"MaxAbsolutePower"}; break;
+        case duck::vt::Property::SIZE : return {"Size"}; break;
+        case duck::vt::Property::X : return {"X"}; break;
+        case duck::vt::Property::Y : return {"Y"}; break;
+        default: return {"Undefined"}; break;
+    }
+}
+
 duck::vt::ValueTree::ValueTree() {
 
 }
 
 void duck::vt::ValueTree::create() {
     // create new one
-    vtRoot = juce::ValueTree{"HentaiDuckRoot"};
-    juce::ValueTree curve{"CurveData"};
-    juce::ValueTree points{"NormalizedPoints"};
+    vtRoot = juce::ValueTree{getIDFromType(duck::vt::Tree::ROOT)};
+    juce::ValueTree curve{getIDFromType(duck::vt::Tree::CURVE_DATA)};
+    juce::ValueTree points{getIDFromType(duck::vt::Tree::NORMALIZED_POINTS)};
     curve.appendChild(points, &undoManager);
     vtRoot.appendChild(curve, &undoManager);
 
-    addPoint({0.f, 1.f}, 10.f, 50.f, 20.f);
+    addPoint({0.f, 0.f}, -10.f, 50.f, 20.f);
+    addPoint({0.002f, 1.f}, 8.f, 50.f, 20.f);
+    addPoint({0.3f, 0.3f}, -8.f, 50.f, 20.f);
+    addPoint({0.5f, 0.f}, 0.f, 50.f, 20.f);
     addPoint({1.f, 0.f}, 0.f, 50.f, 20.f);
-
-    createXML("C:/Dev/Juce Projects/HentaiDuck/tree.xml");
 }
 
 void duck::vt::ValueTree::addListener(juce::ValueTree::Listener* listener) {
@@ -55,17 +78,21 @@ void duck::vt::ValueTree::createXML(const juce::String& pathToFile) const {
 }
 
 void duck::vt::ValueTree::addPoint(const juce::Point<float>& coords, const float& power, const float& maxAbsPower, const float& size) {
-    auto curve = vtRoot.getOrCreateChildWithName("CurveData", &undoManager);
-    auto points = curve.getOrCreateChildWithName("NormalizedPoints", &undoManager);
-    if (!points.isValid()) return;
-    juce::ValueTree point{"Point"};
-    juce::ValueTree xy{"Coords"};
+    using vt = duck::vt::ValueTree;
+    using tree = duck::vt::Tree;
+    using prop = duck::vt::Property;
 
-    xy.setProperty("X", {double(coords.x)}, &undoManager);
-    xy.setProperty("Y", {double(coords.y)}, &undoManager);
-    point.setProperty("Power", {double(power)}, &undoManager);
-    point.setProperty("MaxAbsolutePower", {double(maxAbsPower)}, &undoManager);
-    point.setProperty("Size", {double(size)}, &undoManager);
+    auto curve = vtRoot.getOrCreateChildWithName(getIDFromType(tree::CURVE_DATA), &undoManager);
+    auto points = curve.getOrCreateChildWithName(getIDFromType(tree::NORMALIZED_POINTS), &undoManager);
+    if (!points.isValid()) return;
+    juce::ValueTree point{getIDFromType(tree::POINT)};
+    juce::ValueTree xy{getIDFromType(tree::COORDS)};
+
+    xy.setProperty(getIDFromType(prop::X), {double(coords.x)}, &undoManager);
+    xy.setProperty(getIDFromType(prop::Y), {double(coords.y)}, &undoManager);
+    point.setProperty(getIDFromType(prop::POWER), {double(power)}, &undoManager);
+    point.setProperty(getIDFromType(prop::MAX_ABSOLUTE_POWER), {double(maxAbsPower)}, &undoManager);
+    point.setProperty(getIDFromType(prop::SIZE), {double(size)}, &undoManager);
 
     point.appendChild(xy, &undoManager);
     points.appendChild(point, &undoManager);
@@ -79,23 +106,17 @@ const::juce::ValueTree& duck::vt::ValueTree::getRoot() const {
 
 bool duck::vt::ValueTree::copyFrom(const void* data, int sizeInBytes){
     vtRoot = juce::ValueTree::readFromData(data, sizeInBytes);
-    std::cout << "restored plugin state\n";
     return vtRoot.isValid();
 }
 
 void duck::vt::ValueTree::writeToStream(juce::OutputStream& stream) const {
     vtRoot.writeToStream(stream);
-    std::cout << "wrote plugin state\n";
 }
 
 void duck::vt::ValueTree::setChild(const duck::vt::Tree& type, juce::ValueTree& toTree) {
-    
     const auto id = getIDFromType(type);
-
     auto oldTree = getChildRecursive(id, vtRoot);
 
-    // auto idx = vtRoot.indexOf(vtRoot.getChildWithName(id));
-    // vtRoot.removeChild(idx, &undoManager);
     if (oldTree != nullptr && oldTree->isValid()){
         auto parent = oldTree->getParent();
         if (parent.isValid()){
@@ -105,7 +126,6 @@ void duck::vt::ValueTree::setChild(const duck::vt::Tree& type, juce::ValueTree& 
                 parent.appendChild(toTree, &undoManager);
             }
         }
-        // oldTree->copyPropertiesAndChildrenFrom(toTree, &undoManager);
     }
 }
 
@@ -128,24 +148,3 @@ juce::ValueTree* duck::vt::ValueTree::getChildRecursive(const juce::Identifier& 
     return oldTree;
 }
 
-juce::Identifier duck::vt::ValueTree::getIDFromType(const duck::vt::Tree& tree) {
-    switch (tree) {
-        case duck::vt::Tree::ROOT : return {"Root"}; break;
-        case duck::vt::Tree::CURVE_DATA : return {"CurveData"}; break;
-        case duck::vt::Tree::NORMALIZED_POINTS : return {"NormalizedPoints"}; break;
-        case duck::vt::Tree::POINT : return {"Point"}; break;
-        case duck::vt::Tree::COORDS : return {"Coords"}; break;
-        default: return {"Undefined"}; break;
-    }
-}
-
-juce::Identifier duck::vt::ValueTree::getIDFromType(const duck::vt::Property& property) {
-    switch (property) {
-        case duck::vt::Property::POWER : return {"Power"}; break;
-        case duck::vt::Property::MAX_ABSOLUTE_POWER : return {"MaxAbsolutePower"}; break;
-        case duck::vt::Property::SIZE : return {"Size"}; break;
-        case duck::vt::Property::X : return {"X"}; break;
-        case duck::vt::Property::Y : return {"Y"}; break;
-        default: return {"Undefined"}; break;
-    }
-}
--- Source/Common/ValueTreeManager.h
+++ Source/Common/ValueTreeManager.h
@@ -26,6 +26,7 @@ enum class Property {
 
 class ValueTree {
 public:
+    // creates an empty, invalid tree
     ValueTree();
     ~ValueTree(){};
 
@@ -36,20 +37,28 @@ public:
     
     // makes a new root tree
     void create();
-
+    
+    // returns if the underlying root tree is valid
     bool isValid() {return vtRoot.isValid();}
+    // adds a listener to the root tree
     void addListener(juce::ValueTree::Listener* listener);
+    // returns the undo manager used for all things
     juce::UndoManager* getUndoManager() {return &undoManager;}
-
+    
+    // replaces the current tree from the tree found in the data if possible.
     bool copyFrom(const void* data, int sizeInBytes);
+    // writes the current tree to an output stream
     void writeToStream(juce::OutputStream& stream) const;
     // writes the root tree structure to an XML file. Example path: "C:/Dev/tree.xml"
     void createXML(const juce::String& pathToFile) const;
 
     // functions for the curve class
     void addPoint(const juce::Point<float>& coords, const float& power, const float& maxAbsPower, const float& size);
+    // returns the root
     const juce::ValueTree& getRoot() const;
+    // removes the first child matching the type, and replacing it with tree if possible. This happens recursively though all children (not optimized)
     void setChild(const duck::vt::Tree& type, juce::ValueTree& toTree);
+    // recursively looks for a sub-tree matching the ID (not optimized)
     juce::ValueTree* getChildRecursive(const juce::Identifier& id, juce::ValueTree& tree);
     
 private:
